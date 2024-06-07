beaf5aeba27816106ce52541a88fa896503e3d67
Micky22 <Woodhercules22@gmail.com>
2024-06-07 09:35:53 +0200

    version 10
    cambios de version

--- pages/verReceta.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$id_receta = $_GET['id'];
-// Luego puedes usar $id_receta para hacer consultas o realizar cualquier acción necesaria en la página verReceta.php
-echo "AQUI SE MOSTRARA TU RECETA que es la :".$id_receta;
-
-?>
